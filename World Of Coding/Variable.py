89ebe94329ff939ec5e9bab0b8390150ce673a47
starwar1799 <starwar1799@gmail.com>
2026-07-14 07:51:57 -0500

    variable

---
+++ World Of Coding/Variable.py
@@ -0,0 +1,12 @@
+x = 88888888888888888
+y = "bob"
+print(x)
+print(y)
+
+
+codingal = "codingal"
+print(codingal)
+
+name = input("enter your name: ")
+
+print("\nHello", name, "\nwelcome to codingal")
