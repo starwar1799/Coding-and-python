c4cae5460e100945ede7b989caf67318a16168d5
starwar1799 <starwar1799@gmail.com>
2026-07-14 16:14:16 -0500

    birthdayPlanner

---
+++ Birthday planner/birthdayPlanner.py
@@ -0,0 +1,12 @@
+#Raj's friends
+bob = "June 8"
+jim = "july 8"
+jeff = "may 8"
+johnny = "april 8"
+timmy = "march 8"
+
+print("bob's birthday is:", bob)
+print("jim's birthday is:", jim)
+print("jeff's birthday is:", jeff)
+print("johnny's birthday is:", johnny)
+print("timmy's birthday is:", timmy)
