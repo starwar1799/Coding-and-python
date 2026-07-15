15b308c647665b804ebee72f3e0bbf132357a938
starwar1799 <starwar1799@gmail.com>
2026-07-15 08:12:43 -0500

    data_type.py

---
+++ data type/datatype.py
@@ -0,0 +1,21 @@
+name= input("enter real name:")
+gadget = input("enter favorite gadget:")
+
+agent_number = 7
+speed_rating = 8.6
+mission_count = 14
+height_m = 5.75
+is_active  = True
+
+print("Name:", name, "-> type:", type(name))
+print("Gadget:", gadget, "-> type:", type(gadget))
+print("Agent Number:", agent_number, "-> type:", type(agent_number))
+print("Speed Rating:", speed_rating, "-> type:", type(speed_rating))
+print("Mission Count:", mission_count, "_> type:", type(mission_count))
+print("Height:", height_m, "-> type:", type(height_m))
+print("Is Active:", is_active, "-> type:", type(is_active))
+
+agent_number_text = str(agent_number)
+mission_count_text = str(mission_count)
+speed_rating_text = str(speed_rating)
+status_text = str(is_active)
