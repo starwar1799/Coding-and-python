924e3d0fb5fe723445fbb2bd523f3b2e63803513
starwar1799 <starwar1799@gmail.com>
2026-07-24 07:34:07 -0500

    conditionals.py

--- operators3/homeworkOperator3.py
+++ operators3/homeworkOperator3.py
@@ -16,3 +16,15 @@ print(f"ASCII Value: {ascii_val}")
 
 
 print("\nCharacter Type: ", end="")
+if ascii_val >= 65 and ascii_val <=90:
+    print("Uppercase Letter")
+elif ascii_val >= 97 and ascii_val <= 122:
+    print("Lowercase Letter")
+elif ascii_val >= 48 and ascii_val <= 57:
+    print("Digit")
+elif ascii_val == 32:
+    print("Space") 
+else:
+    print("Special Character")
+
+
