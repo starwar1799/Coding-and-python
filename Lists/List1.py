05fba71fd13b7fdf6c609ffd20f6ebcf1fca8f8e
starwar1799 <starwar1799@gmail.com>
2026-08-26 19:22:46 -0500

    rewtyuiop

---
+++ Lists/List1.py
@@ -0,0 +1,14 @@
+empty_list = []
+print(empty_list)
+
+numbers = [1, 2, 3, 4, 5, 6,]
+print(numbers)
+
+triples = [1, 2, 3] *3
+print(triples)
+
+aList = [600, 1200, 1800, 2400, 3000]
+string="Hello good evening"
+print("Reverse the given string", string[::-1])
+aList = aList[::-1]
+print(aList,"\n")
