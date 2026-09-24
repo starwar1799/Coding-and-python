69ac70298c59ee95bcb23e1f7bcb43497fcef3ad
starwar1799 <starwar1799@gmail.com>
2026-09-23 19:25:55 -0500

    aecfdjjhfjkwsecfjnwefjmnefjmaswjmasdkmazswmklaswekmnaszk,zsk,izsdeklisedkszdk,cvf

---
+++ OOP/Cv.py
@@ -0,0 +1,13 @@
+class Vehicle:
+
+
+        def __init__(self, max_speed, mileage):
+
+
+                self.max_speed = max_speed
+                self.mileage = mileage
+
+mode1x = Vehicle(240, 18)
+
+print("Model Max Speed: ",mode1x.max_speed)
+print("Model Mileage: ",mode1x.mileage)
